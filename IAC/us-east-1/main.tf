688a406b53c7f5df423760329a23460ff26e0df2
Team4techSolutionsClass7 <talk2calson.njinoh@gmail.com>
2024-12-04 13:45:12 -0500

    Update main.tf

--- IAC/us-east-1/main.tf
+++ IAC/us-east-1/main.tf
@@ -1,9 +1,11 @@
+
+provider "aws" {
+  region = "us-east-1" 
+}
+
 module "ec2_instance" {
   source        = "./terraform-ec2-module"
-  ami_id        = "ami-0c55b159cbfafe1f0" # Replace with your AMI ID
+  ami_id        = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  key_name      = "my-key-pair"
-  subnet_id     = "subnet-0123456789abcdef"
   instance_name = "MyInstance"
-  region        = "us-east-1"
 }
