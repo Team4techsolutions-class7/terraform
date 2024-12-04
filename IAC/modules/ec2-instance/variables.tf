dd3c48254882d97caf2e181f73261aad9521e735
Team4techSolutionsClass7 <talk2calson.njinoh@gmail.com>
2024-12-04 16:02:40 -0500

    Create variables.tf

---
+++ IAC/modules/ec2-instance/variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "The name tag for the EC2 instance"
+  type        = string
+  default     = "MyEC2Instance"
+}
