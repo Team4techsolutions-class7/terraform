74ea909409d082983a86950ceecd86c553de4c2c
Team4techSolutionsClass7 <talk2calson.njinoh@gmail.com>
2024-12-04 16:14:46 -0500

    Update main.tf

--- IAC/ca-central-1/main.tf
+++ IAC/ca-central-1/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source        = "../../modules/ec2-instance"
+  source        = "../modules/ec2-instance"
   ami_id        = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   instance_name = "Jenkins"
