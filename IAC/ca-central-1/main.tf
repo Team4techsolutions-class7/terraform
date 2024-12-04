a6e9e8b9e846128ff552d59309ac583b2bdfc038
Team4techSolutionsClass7 <talk2calson.njinoh@gmail.com>
2024-12-04 15:33:26 -0500

    Update main.tf

--- IAC/ca-central-1/main.tf
+++ IAC/ca-central-1/main.tf
@@ -7,5 +7,5 @@ module "ec2_instance" {
   source        = "./terraform-ec2-module"
   ami_id        = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  instance_name = "MyInstance"
+  instance_name = "Jenkins"
 }
