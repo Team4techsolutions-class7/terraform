a165eb60121d85a7023ebef312d52e37e31f26c1
Team4techSolutionsClass7 <talk2calson.njinoh@gmail.com>
2024-12-16 00:58:08 -0500

    Update main.tf

--- IAC/staging/main.tf
+++ IAC/staging/main.tf
@@ -1,1 +1,27 @@
+terraform {
+  required_version = ">= 1.3.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = ca-central-1
+
+  assume_role {
+    role_arn     = "arn:aws:iam::257394485697:role/TerraformExecutionRole"
+    session_name = "terraform-session"
+  }
+}
+
+
+module "ec2_instance" {
+  source        = "../modules/ec2-instance"
+  ami_id        = "ami-0c9f6749650d5c0e3"
+  instance_type = "t2.micro"
+  instance_name = "Jenkins"
+}
