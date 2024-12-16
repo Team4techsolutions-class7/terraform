b7e345007ddcdf68850fd260235fadf0d03e7190
Team4techSolutionsClass7 <talk2calson.njinoh@gmail.com>
2024-12-16 11:29:12 -0500

    Create main.tf

---
+++ IAC/production/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ca-central-1"
+}
+
+module "ec2_instance" {
+  source        = "../modules/ec2-instance"
+  ami_id        = "ami-0c9f6749650d5c0e3"
+  instance_type = "t2.micro"
+  instance_name = "Jenkins"
+}
