bbcdd6d388001e89d38744137e41f66a5a4d602c
Team4techSolutionsClass7 <talk2calson.njinoh@gmail.com>
2024-12-18 04:56:35 -0500

    Update main.tf

--- IAC/dev/main.tf
+++ IAC/dev/main.tf
@@ -13,16 +13,10 @@ provider "aws" {
   region = "ca-central-1"
 }
 
-assume_role {
-    role_arn     = "arn:aws:iam::257394485697:role/TerraformExecutionRole"
-    session_name = "terraform-session"
-  }
-}
-
 module "ec2_instance" {
   source        = "../modules/ec2-instance"
   ami_id        = "ami-0c9f6749650d5c0e3"
   instance_type = "t2.micro"
   instance_name = "Jenkins"
 
-#jenkins
+
