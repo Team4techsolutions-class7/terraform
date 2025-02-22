c6e410d2713a0e00d930005b97e17798683ad5af
Team4techSolutionsClass7 <talk2calson.njinoh@gmail.com>
2025-02-22 10:48:33 -0500

    Update main.tf

--- IAC/modules/ec2-instance/main.tf
+++ IAC/modules/ec2-instance/main.tf
@@ -1,8 +1,16 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  
+
   tags = {
     Name = var.instance_name
+  }
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted = true
   }
 }
